55410f57f9d2cb93f722d8321f2de11783d648ec
LucasNithael <lucasnithael0101@gmail.com>
2024-02-13 16:13:20 -0300

    Models PedidoDetalhe e Pedido

--- LanchesMac/Context/AppDbContext.cs
+++ LanchesMac/Context/AppDbContext.cs
@@ -12,6 +12,7 @@ namespace LanchesMac.Context
         public DbSet<Categoria> Categoria { get; set; }
         public DbSet<Lanche> Lanche { get; set; }
         public DbSet<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
-        
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoDetalhe> PedidoDethales { get; set; }
     }
 }
