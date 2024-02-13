b261fa5bf57721796d743f4893a4404b6649dcc6
LucasNithael <lucasnithael0101@gmail.com>
2024-02-12 23:48:23 -0300

    ViewComponent de CategoriaMenu

---
+++ LanchesMac/Components/CategoriaMenu.cs
@@ -0,0 +1,21 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace LanchesMac.Components
+{
+    public class CategoriaMenu: ViewComponent
+    {
+        private ICategoriaRepository _categoriaRepository;
+        public CategoriaMenu(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var categorias = _categoriaRepository.Categorias.OrderBy(p => p.Nome);
+
+            return View(categorias);
+        }
+    }
+}
